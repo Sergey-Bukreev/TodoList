4b1c44506207fee1a1cc0b53a5e49c85e62dd0e4
BukreevSergey <bukreevwork2@gmail.com>
2023-12-12 19:34:35 +0200

    refactor to filter each card separately

--- src/todolist/TodoList.tsx
+++ src/todolist/TodoList.tsx
@@ -1,85 +1,86 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { ToDoCard } from './todocard/ToDoCard';
 
-import {ToDoCard} from "./todocard/ToDoCard";
+export type FilterValuesType = 'all' | 'active' | 'completed';
 
-export type FilterValuesType = "all" | "active" | "completed"
 let TodoCardData = [
     {
-        id:1,
-        title: "What to learn",
-        filter:'completed',
-        task:[
-            {id:1, title:"HTML", isDone:true},
-            {id:2, title:"JS/ES6", isDone:true},
-            {id:3, title:"React", isDone:false}
+        id: 1,
+        title: 'What to learn',
+        filter: 'all',
+        todoList: [
+            { id: 1, title: 'HTML', isDone: true },
+            { id: 2, title: 'JS/ES6', isDone: true },
+            { id: 3, title: 'React', isDone: false },
         ],
-
-
     },
     {
-        id:2,
-        title: "What to by",
+        id: 2,
+        title: 'What to buy',
         filter: 'all',
-        task:[
-            {id:1, title:"Beer", isDone:false},
-            {id:2, title:"Cheeps", isDone:true},
-            {id:3, title:"Dry Fish", isDone:false}
+        todoList: [
+            { id: 1, title: 'Beer', isDone: false },
+            { id: 2, title: 'Chips', isDone: true },
+            { id: 3, title: 'Dry Fish', isDone: false },
         ],
-
-    }
-]
-
-
+    },
+];
 
 export const TodoList: React.FC = () => {
     let [tasks, setTasks] = useState(TodoCardData);
-    let [filter, setFilter] = useState<FilterValuesType>("all")
+    let [filter, setFilter] = useState<FilterValuesType>('all');
 
     const removeTask = (cardId: number, taskId: number) => {
         const updatedTasks = tasks.map((el) => {
-            if(cardId === el.id) {
-
-                const updatedTask = el.task.filter((task) => task.id !== taskId);
-                return { ...el, task: updatedTask };
+            if (cardId === el.id) {
+                const updatedTask = el.todoList.filter((task) => task.id !== taskId);
+                return { ...el, todoList: updatedTask };
             }
-            return el
+            return el;
         });
 
         setTasks(updatedTasks);
-    }
-
-    const changeFilter = (value: FilterValuesType) => {
-        setFilter(value);
     };
 
+    const changeFilter = (value: FilterValuesType, cardId: number) => {
+        setFilter(value);
+
+        const updatedTasks = tasks.map((el) => {
+            if (cardId === el.id) {
+                return { ...el, filter: value };
+            }
+            return el;
+        });
+
+        setTasks(updatedTasks);
+    };
 
     return (
-            <>
-                {tasks.map((el)=> {
-                    const filteredTasks = el.task.filter((el) => {
-                       if (filter === "active") {
-                            return !el.isDone
-                        } else if (filter === "completed") {
-                            return  el.isDone
-                        }
+        <>
+            {tasks.map((el) => {
+                const filteredTasks = el.todoList.filter((task) => {
+                    if (el.filter === 'active' && !task.isDone) {
+                        return true;
+                    } else if (el.filter === 'completed' && task.isDone) {
+                        return true;
+                    } else if (el.filter === 'all') {
+                        return true;
+                    }
 
-                       return true;
-                    })
+                    return false;
+                });
 
-                    return <ToDoCard title={el.title}
-                                     task={filteredTasks}
-                                     id={el.id}
-                                     key={el.id}
-                                     removeTask={(tasks) => removeTask(el.id,tasks)}
-                                     changeFilter={changeFilter}
+                return (
+                    <ToDoCard
+                        title={el.title}
+                        task={filteredTasks}
+                        id={el.id}
+                        key={el.id}
+                        removeTask={(taskId) => removeTask(el.id, taskId)}
+                        changeFilter={(value) => changeFilter(value, el.id)}
                     />
-                })}
-            </>
-    )
-}
-
-
-
-
-
-
+                );
+            })}
+        </>
+    );
+};
